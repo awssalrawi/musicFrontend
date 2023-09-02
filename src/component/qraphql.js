38e5764255cfc515fd891df8bb7f0aaa7a9f9961
Awss Nafea <awss.alrawi@gmail.com>
2023-09-02 18:18:57 +0300

    first commit

--- src/component/qraphql.js
+++ src/component/qraphql.js
@@ -1,4 +1,5 @@
 import { gql } from "@apollo/client";
+import { SchemaMetaFieldDef } from "graphql";
 
 export const GET_ALL_SONGS = gql`
   query {
@@ -13,3 +14,27 @@ export const GET_ALL_SONGS = gql`
     }
   }
 `;
+// Check schema.py
+
+// delete
+// mutation{
+//     deleteItem(itemType: "artist", itemId: "1") {
+//    success
+//  }
+// }
+// get by id
+// {
+//     song(id: "1") {
+//       id
+//       title
+//     }
+//   }
+// create
+// mutation {
+//     create_artist(name: "John Doe") {
+//       artist {
+//         id
+//         name
+//       }
+//     }
+//   }
